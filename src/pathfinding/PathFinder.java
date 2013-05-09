15cf8e36ab6362966029ff0973ed93313160c58b
Corentin Legros <corentin.legros@gmail.com>
2013-05-09 21:24:13 +0200

    Premier jet de l'algorithme de pathfinding

--- src/pathfinding/PathFinder.java
+++ src/pathfinding/PathFinder.java
@@ -1,18 +1,40 @@
 package pathfinding;
 
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import java.util.AbstractMap.SimpleEntry;
+
+import org.newdawn.slick.util.Log;
+
+import tools.Vector2i;
+
+import map.Map;
 
 public class PathFinder {
 	
-	protected HashSet<Node> openedList;
-	protected HashSet<Node> closedList;
+	protected HashMap<SimpleEntry<Integer, Integer>, Node> openedList;
+	protected HashMap<SimpleEntry<Integer, Integer>, Node> closedList;
+	
+	protected Vector2i startPoint;
+	protected Vector2i endPoint;
+	
+	protected Map refMap;
+	
+	public PathFinder(Map refMap) {
+		this.refMap = refMap;
+		
+		openedList = new HashMap<SimpleEntry<Integer, Integer>, Node>();
+		closedList = new HashMap<SimpleEntry<Integer, Integer>, Node>();
+	}
 	
 	
-	
-	public void computePath(Node startNode, Node endNode) {
+	public ArrayList<Node> computePath(Vector2i start, Vector2i end, int mapType) {
 		
 		/*
-		 * 
 		 * Liste ouverte : liste des noeuds étudiés
 		 * Liste fermée : liste des noeuds solutions
 		 * 
@@ -46,5 +68,138 @@ public class PathFinder {
 		 * -> Sinon, on l'ajoute dans la liste ouverte et on calcule sa qualité.
 		 * 
 		 */
+		
+		startPoint = new Vector2i(start);
+		endPoint = new Vector2i(end);
+		
+		if (!refMap.hasPath(start.x, start.y, end.x, end.y, mapType)) {
+			return null;
+		}
+		
+		SimpleEntry<Integer, Integer> currentPosition = new SimpleEntry<Integer, Integer>(startPoint.x, startPoint.y);
+		Node startNode = new Node();
+		startNode.parent = new SimpleEntry<Integer, Integer>(0, 0);
+		
+		openedList.put(currentPosition, startNode);
+		addToClosedList(currentPosition);
+		storeAdjacentNodes(currentPosition, mapType);
+		
+		// tant qu'on est pas à la fin du trajet voulu ET que la liste des potentiels sommets n'est pas vide
+		while (currentPosition.getKey() != endPoint.x && currentPosition.getValue() != endPoint.y && !openedList.isEmpty()) {
+			currentPosition = findBestNodeOfList(openedList);
+			addToClosedList(currentPosition);
+			storeAdjacentNodes(currentPosition, mapType);
+			
+			if (currentPosition.getKey() == endPoint.x && currentPosition.getValue() == endPoint.y) {
+				System.out.println("Il semblerait que la fin ait été atteinte :)");
+			}
+		}
+		
+		// tmp
+		return new ArrayList<Node>();
+	}
+	
+	/*
+	 * Cherche les voisins et les stocke selon leur valeurs respectives
+	 * @param Node currentPosition
+	 * @param int mapType
+	 */
+	private void storeAdjacentNodes(SimpleEntry<Integer, Integer> currentPosition, int mapType) {
+		
+		SimpleEntry<Integer, Integer> tmpEntry = null;
+		Node tmpNode = null;
+		
+		boolean isInMap = false;
+		boolean isCurrentNode = false;
+		
+		for (int i = currentPosition.getKey() - 1 ; i < currentPosition.getKey() + 1 ; ++i) {
+			for (int j = currentPosition.getValue() - 1 ; j < currentPosition.getValue() + 1 ; ++j) {
+				
+				isInMap = i >= 0 && i < refMap.getWidth() && j >= 0 && j < refMap.getHeight();
+				isCurrentNode = i == currentPosition.getKey() && j == currentPosition.getValue();
+				
+				// case testée existante et != noeud du centre
+				if (!isInMap && !isCurrentNode) {
+					
+					if (!refMap.isTileBlocked(currentPosition.getKey(), currentPosition.getValue(), mapType)) {
+						tmpEntry = new SimpleEntry<Integer, Integer>(currentPosition.getKey(), currentPosition.getValue());
+						
+						if (!closedList.containsKey(tmpEntry)) {
+							
+							tmpNode = new Node();
+							tmpNode.costFromStartToNode = closedList.get(currentPosition).costFromStartToNode 
+									+ computeNodeDistance(currentPosition.getKey(), currentPosition.getValue(), i, j);
+							tmpNode.costFromNodeToEnd = computeNodeDistance(currentPosition.getKey(), currentPosition.getValue(), endPoint.x, endPoint.y);
+							tmpNode.costSum = tmpNode.costFromStartToNode + tmpNode.costFromNodeToEnd;
+							
+							// si liste ouverte contient déjà le noeud
+							if (openedList.containsKey(tmpEntry)) {
+								// si le nouveau chemin est plus avantageux que l'ancien, on le remplace
+								if (tmpNode.costSum < openedList.get(tmpEntry).costSum) {
+									openedList.put(tmpEntry, tmpNode);
+								}
+								
+								// sinon on stocke le noeud comme étant déjà étudié
+							} else {
+								openedList.put(tmpEntry, tmpNode);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	/*
+	 * Permet de calculer la distance entre 2 points
+	 * 
+	 * @param xStart
+	 * @param yStart
+	 * @param xEnd
+	 * @param yEnd
+	 * @return
+	 */
+	private int computeNodeDistance(int xStart, int yStart, int xEnd, int yEnd) {
+		return (int) Math.sqrt(Math.pow((double) xStart - xEnd, 2.0) + Math.pow((double) yStart - yEnd, 2.0));
+	}
+	
+	/*
+	 * Permet de déterminer quel noeud est le plus performant dans une liste
+	 * @param listToSearchIn
+	 * @return
+	 */
+	private SimpleEntry<Integer, Integer> findBestNodeOfList(HashMap<SimpleEntry<Integer, Integer>, Node> listToSearchIn) {
+		
+		int maxCost = 1000;
+		Entry<SimpleEntry<Integer, Integer>, Node> tmpEntry;
+		SimpleEntry<Integer, Integer> tmpPosition = null;
+		
+		
+		Set<Entry<SimpleEntry<Integer, Integer>, Node>> tmpSet = listToSearchIn.entrySet();
+		
+		Iterator<Entry<SimpleEntry<Integer, Integer>, Node>> it = tmpSet.iterator();
+		
+		while (it.hasNext()) {
+			tmpEntry = it.next();
+			if (tmpEntry.getValue().costSum < maxCost) {
+				maxCost = tmpEntry.getValue().costSum;
+				tmpPosition = tmpEntry.getKey();
+			}
+		}
+		
+		return tmpPosition;
+	}
+	
+	/*
+	 * Permet de passer une position de la liste ouverte à la liste fermée
+	 * @param SimpleEntry<Integer, Integer> position clé du noeud à passer
+	 */
+	private void addToClosedList(SimpleEntry<Integer, Integer> position) {
+		Node tmpNode = openedList.get(position);
+		closedList.put(position, tmpNode);
+		
+		if (openedList.remove(position) == null) {
+			Log.warn("[A* algorithm] Unable to remove an old position (opened to closed list)");
+		}
 	}
 }
