feaf60d5a092b386d050877358cd3d246b32e6b6
Corentin Legros <corentin.legros@gmail.com>
2013-06-07 00:08:22 +0200

    Le gestionnaire d'animation gère désormais les animations des tours (mais de façon sale)

--- src/resources/AnimationManager.java
+++ src/resources/AnimationManager.java
@@ -36,15 +36,26 @@ public class AnimationManager extends AbstractManager<Animation> {
 		
 		Iterator<String> it = spriteSheetKeys.iterator();
 		StringBuilder sbKey = new StringBuilder();
+		SpriteSheet tmpSpS = null;
 		while (it.hasNext()) {
 			String key = it.next();
-			for (int i = 0 ; i < 8 ; ++i) {
+			if (key.contains("creature")) {
+				for (int i = 0 ; i < 8 ; ++i) {
+					sbKey.setLength(0);
+					
+					tmpSpS = objSpSManager.get(key);
+					sbKey.append(key).append("_").append(i);
+					// new Animation(tmpSpS, x1, y1, x2, y2, horizontalScan, duration, autoUpdate)
+					data.put(sbKey.toString(), new Animation(tmpSpS, 0, i, 13, i, true, 20, true));
+					System.out.println(sbKey.toString());
+				}
+			} else if (key.contains("tower")) {
 				sbKey.setLength(0);
 				
-				SpriteSheet tmpSpS = objSpSManager.get(key);
-				sbKey.append(key).append("_").append(i);
-				// new Animation(tmpSpS, x1, y1, x2, y2, horizontalScan, duration, autoUpdate)
-				data.put(sbKey.toString(), new Animation(tmpSpS, 0, i, 13, i, true, 20, true));
+				tmpSpS = objSpSManager.get(key);
+				sbKey.append(key).append("_").append(0);
+				
+				data.put(sbKey.toString(), new Animation(tmpSpS, 0, 0, 0, 0, true, 20, true));
 			}
 		}
 	}
