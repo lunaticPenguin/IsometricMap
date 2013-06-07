61421bb439d4b800c1a4ab80c69e2676300ded93
Corentin Legros <corentin.legros@gmail.com>
2013-06-07 02:12:05 +0200

    La classe TowerGuard est capable d'initialiser les objets Towers, ainsi que de récupérer toutes les animations correspondantes.

--- src/entities/types/buildings/TowerGuard.java
+++ src/entities/types/buildings/TowerGuard.java
@@ -5,7 +5,11 @@ import java.util.HashMap;
 
 import org.newdawn.slick.Animation;
 import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.geom.Vector2f;
 import org.newdawn.slick.state.StateBasedGame;
+
+import resources.AnimationManager;
+import tools.Vector2i;
 
 import entities.AbstractBuildingEntity;
 import entities.AbstractEntity;
@@ -21,13 +25,51 @@ public class TowerGuard extends AbstractBuildingEntity implements IAggressive {
 	 */
 	protected static HashMap<String, ArrayList<Animation>> animationsStates;
 	
+	public TowerGuard() {
+		super();
+		
+		s = new Vector2f();
+		m = new Vector2i();
+		
+		displayingOffset.x = -32;
+		displayingOffset.y = -97;
+		
+		zoneDim.x = 64;
+		zoneDim.y = 112;
+		zoneOffset.x = 20;
+		zoneOffset.y = 28;
+		
+		init();
+	}
 	
 	@Override
 	public void init() {
 		attack = 10;
 		defend = 15;
+		this.direction = DIRECTION_NORTH;
+		this.state = STATE_HEALTHY;
+		
+		AnimationManager objAnimationManager = AnimationManager.getInstance();
+		
+		if (animationsStates == null) {
+			animationsStates = new HashMap<String, ArrayList<Animation>>();
+			
+			String statesKeys[] = new String[]{"healthy", "damaged", "destroyed"};
+			String longStatesKeys[] = new String[]{"tower.guard.healthy", "tower.guard.damaged", "tower.guard.destroyed"};
+			StringBuilder tmpSBKey = new StringBuilder();
+			
+			ArrayList<Animation> stateAnimations = new ArrayList<Animation>();
+			
+			for (int i = 0 ; i < statesKeys.length ; ++i) {
+				tmpSBKey.append(longStatesKeys[i]).append("_").append(0);
+				
+				stateAnimations.add(objAnimationManager.get(tmpSBKey.toString()));
+				tmpSBKey.setLength(0);
+				animationsStates.put(statesKeys[i], stateAnimations);
+			}
+		}
 	}
-
+	
 	@Override
 	public void doAction() {
 		
