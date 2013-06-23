7ef4bfe3f1ccd2b915a4672772f665a0be24c9d8
Corentin Legros <corentin.legros@gmail.com>
2013-06-23 23:04:45 +0200

    Mise en place d'une factory et d'un manager de projectiles (ils se correspondent)

---
+++ src/entities/manager/ProjectileManager.java
@@ -0,0 +1,59 @@
+package entities.manager;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.state.StateBasedGame;
+
+import entities.factory.ProjectileFactory;
+import entities.types.projectiles.AbstractProjectile;
+
+/**
+ * Le manager de projectile contient bien 2 listes : une affichée, une masquée
+ * mais n'utilise que celle affichée
+ * @author co
+ *
+ */
+public class ProjectileManager extends AbstractManager<AbstractProjectile> {
+	
+	protected static ProjectileManager instance;
+	
+	public static ProjectileManager getInstance() {
+		if (instance == null) {
+			instance = new ProjectileManager();
+		}
+		return instance;
+	}
+	
+	protected ProjectileManager() {
+		factory = ProjectileFactory.getInstance();
+		data = new ArrayList<AbstractProjectile>();
+	}
+
+	@Override
+	protected void renderEntity(Graphics g, AbstractProjectile projectile) {
+		if (projectile.belongToRenderedAera()) {
+			projectile.draw(g);
+		}
+	}
+	
+	@Override
+	public void update(GameContainer container, StateBasedGame game, int delta) {
+		
+		Iterator<AbstractProjectile> dataIterator = data.iterator();
+		AbstractProjectile projectile;
+		
+		dataIterator = data.iterator();
+		while (dataIterator.hasNext()) {
+			projectile = dataIterator.next();
+			
+			// si projectile détruit
+			if (!projectile.update(container, game, delta)) {
+				dataIterator.remove();
+				factory.setEntityBack("cannonball", projectile);
+			}
+		}
+	}
+}
